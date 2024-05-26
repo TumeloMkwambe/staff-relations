8af200a9e7fdc405d007f0f120ffdfbb0f66b2d7
TumeloMkwambe <2446873@students.wits.ac.za>
2024-05-26 14:05:39 -0700

    SJDN

--- views/homepage/script.js
+++ views/homepage/script.js
@@ -2,5 +2,5 @@ const body = document.querySelector('body');
 const login = document.querySelector('button');
 login.addEventListener('click', () => {
     body.style.background = "radial-gradient(circle, #305673, #305673, #fcfcfc)";
-    window.location.href = 'http://localhost:3000/signin';
+    window.location.href = 'https://staffrelations2024.azurewebsites.net/signin';
 });
